f4b051a873c68b74e2ce08a0bc431e17fe4c1fcf
Mark Ryan <markrryan99@gmail.com>
2022-05-11 06:30:08 -0700

    starting on routers

---
+++ routes/AppRouter.js
@@ -0,0 +1,8 @@
+// const Router = require('express').Router()
+// const UserRouter = require('./UserRouter.js')
+// const PetRouter = require('./PetRouter.js')
+// const UserRatingRouter = require('./UserRatingRouter.js')
+// Router.use('/users', UserRouter)
+// Router.use('/pets', PetRouter)
+// Router.use('/userratings',UserRatingRouter)
+// module.exports = Router
