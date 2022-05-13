ea85e33b5c208d63c5374270f5e2caaf61c05ed8
Mark Ryan <markrryan99@gmail.com>
2022-05-13 06:38:18 -0700

    updated userrating seed to reflect new model

--- seeders/20220510235043-userratings.js
+++ seeders/20220510235043-userratings.js
@@ -1,19 +1,36 @@
 'use strict';
-const falso = require('@ngneat/falso')
-const{Pet} = require('../models')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    
-    // const petlist = await Pet.findAll({ raw: true })
-    
-    // const userratings = petlist.map((a) => ({
-    //   userratings: falso.randCatchPhrase(),
-    //   petId: petlist[Math.floor(Math.random() * petlist.length)].id,
-    //   createdAt: new Date(),
-    //   updatedAt: new Date()
-    // }))
-    // await queryInterface.bulkInsert('userratings', userratings)
+   const userratings = [
+     {
+       userrating: '10/10, Pretty cute',
+       createdAt: new Date(),
+       updatedAt: new Date()
+     },
+     {
+       userrating: `11/10, They're Great!`,
+       createdAt: new Date(),
+       updatedAt: new Date()
+     },
+     {
+       userrating: `12/10, Heckin' Adorable`,
+       createdAt: new Date(),
+       updatedAt: new Date()
+     },
+     {
+      userrating: `13/10, I'd vote for them for President`,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      userrating: `14/10, This is the GREATEST Pet I've ever seen!!`,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+   ] 
+
+    await queryInterface.bulkInsert('userratings', userratings)
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete('userratings')
